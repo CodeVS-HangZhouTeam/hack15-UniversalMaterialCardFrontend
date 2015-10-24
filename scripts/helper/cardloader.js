51557b89c3caadde49d299c5f6421f857fd8e9ca
James Swineson <jamesswineson@gmail.com>
2015-10-24 19:38:58 +0800

    add cardloader.loadCardFromJSON method

--- scripts/helper/cardloader.js
+++ scripts/helper/cardloader.js
@@ -4,11 +4,16 @@ define(["jquery", "helper/mustache", "highcharts"], function (jquery, mustache, 
         loadCard: function(destination, template_name, data) {
             jquery.get('templates/' + template_name + '.html', function(template, textStatus, jqXhr) {
                 $(destination).append(mustache.render(template, data));
-                $(".highcharts-container text:contains(Highcharts.com)").hide() // remove Highcharts.com text
+                $(".highcharts-container text:contains(Highcharts.com)").hide(); // remove Highcharts.com text
             });
         },
+        loadCardFromJSON: function(destination, template_name, url) {
+            jquery.getJSON(url, function(json) {
+                cardloader.loadCard(destination, template_name, json);
+            });
+        }
 
-    }
+    };
 
     return cardloader;
 });
