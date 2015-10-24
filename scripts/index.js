de4df04b5097c2ae1dd3b4978fd9d419546680ae
James Swineson <jamesswineson@gmail.com>
2015-10-24 15:56:25 +0800

    card loader finished

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,10 @@
-requirejs(["jquery"], function(jquery) {});
 requirejs(["material/material"], function(material) {});
-requirejs(["helper/mustache"], function(mustache) {});
+requirejs(["jquery", "helper/mustache"], function(jquery, mustache) {
+    var loadCard = function(destination, template_name, data) {
+        jquery.get('templates/' + template_name + '.html', function(template, textStatus, jqXhr) {
+            $(destination).append(mustache.render(template, data));
+        });
+    };
+
+    loadCard(".page-content", "card", {});
+});
