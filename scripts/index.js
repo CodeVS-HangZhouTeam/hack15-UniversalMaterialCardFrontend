557479612383d1b1f8f6d09096252fbdca58d017
James Swineson <jamesswineson@gmail.com>
2015-10-24 17:06:11 +0800

    Highcharts introduced

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,30 @@
-requirejs(["material/material"], function(material) {});
-requirejs(["jquery", "helper/mustache"], function(jquery, mustache) {
+require.config({
+    paths: {
+        require: "require",
+        jquery: "jquery",
+        highcharts: "highcharts",
+        mustache: "helper/mustache",
+        material: "material/material"
+    },
+    shim: {
+        highcharts: {
+            exports: "Highcharts",
+            deps: ["jquery"]
+        },
+        jquery: {
+            exports: "$"
+        },
+        material: {
+        }
+    }
+});
+
+requirejs(["jquery", "helper/mustache", "highcharts"], function(jquery, mustache, Highcharts) {
     var loadCard = function(destination, template_name, data) {
         jquery.get('templates/' + template_name + '.html', function(template, textStatus, jqXhr) {
             $(destination).append(mustache.render(template, data));
         });
     };
 
-    loadCard(".page-content", "card", {});
+    loadCard(".page-content", "demo-card-with-highcharts", {});
 });
